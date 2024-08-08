073ec355d6eefe48263f71c95f95f71c48c2abca
Vituin <124085647+Vituin@users.noreply.github.com>
2024-08-08 20:07:52 +0200

    v1 back

--- routes/accommodation.routes.js
+++ routes/accommodation.routes.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const Accomodation = require("../models/Accommodation.model");
+const Accommodation = require("../models/Accommodation.model");
 const Booking = require("../models/Booking.model");
 
-router.get(`/accommodations`, (req, res) => {
+// GET /accommodations
+router.get(`/accommodations`, (req, res, next) => {
   Accommodation.find()
-    .then((data) => {})
+    .then((data) => res.json(data))
     .catch((err) => {
       res
         .status(500)
         .json({ message: "Could not find all the accommodations" });
+    });
+});
+
+//GET /accommodations/:id
+router.get(`/accommodations/:id`, (req, res, next) => {
+  Accommodation.findById(req.params.id)
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.status(500).json({ message: "Could not find the accommodation" });
+    });
+});
+
+// POST /accommodations
+router.post(`/accommodations`, (req, res, next) => {
+  const accommodation = new Accommodation(req.body);
+  accommodation
+    .save()
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.status(500).json({ message: "Could not create the accommodation" });
     });
 });
 
